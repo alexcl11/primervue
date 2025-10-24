ade0220bc6ee01330b9eab153a914ae8291d2180
alexcl11 <aleecanovaslopez@gmail.com>
2025-10-24 14:24:19 +0200

    up

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,14 @@ import CineComponent from './components/CineComponent.vue'
 import MusicaComponent from './components/MusicaComponent.vue'
 import CicloVida from './components/CicloVida.vue'
 import DirectivasComponent from './components/DirectivasComponent.vue'
+import ParImpar from './components/ParImpar.vue'
 const myRoutes = [
     {path:"/", component: HomeComponent},
     {path: "/cine", component: CineComponent},
     {path:"/musica", component: MusicaComponent},
     {path:"/ciclovida", component: CicloVida},
     {path:"/directivas", component: DirectivasComponent},
+    {path:"/parimpar", component: ParImpar}
 ]
 
 // CREAMOS UNA VARIABLE PARA EL router INDICANDO EL TIPO DE NAVEGACION Y LAS RUTAS
